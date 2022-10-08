9c6b8aeab884e174e1e59a6649e8c28afbbe51d6
Syvit <collin.trumpler@gmail.com>
2022-10-08 11:25:01 -0400

    Scoreboard is now partially functional

--- function.js
+++ function.js
@@ -1,5 +1,11 @@
 const choices = ['rock', 'paper', 'scissors'];
-const buttons = document.querySelectorAll('button')
+const buttons = document.querySelectorAll('button');
+let pScore = localStorage.getItem('playerScore');
+document.querySelector('.playerScore').innerHTML = pScore;
+let cScore = localStorage.getItem('computerScore');
+document.querySelector('.computerScore').innerHTML = cScore;
+let ties = localStorage.getItem('ties');
+document.querySelector('.ties').innerHTML = ties;
 
 function game() {
     playerChoice();
@@ -14,10 +20,9 @@ function playerChoice() {
 }
 
 function playRound(playerSelection) {
-    // const playerSelection = playerChoice();
     const computerSelection = computerChoice();
     const winner = checkWinner(playerSelection, computerSelection);
-    console.log(winner);
+    console.log(winner); 
 }
 
 const computerChoice = () => {
@@ -26,13 +31,40 @@ const computerChoice = () => {
 
 function checkWinner(choiceP, choiceC) {
     if (choiceP === choiceC) {
-        return 'Tie';
+        document.querySelector('.gameResult').innerHTML = "Tie";
+        ties++;
     } else if ((choiceP === "rock" && choiceC === "scissors") ||
         (choiceP === "paper" && choiceC === "rock") ||
         (choiceP === "scissors" && choiceC === "paper")) {
-        return 'Player';
+        document.querySelector('.gameResult').innerHTML = "Player Wins";
+        pScore++;
     } else {
-        return 'Computer';
+        document.querySelector('.gameResult').innerHTML = "Computer Wins";
+        cScore++;
     }
+
+    document.querySelector('.playerScore').innerHTML = pScore;
+    localStorage.setItem('.playerScore', pScore);
+    document.querySelector('.computerScore').innerHTML = cScore;
+    localStorage.setItem('.computerScore', cScore);
+    document.querySelector('.ties').innerHTML = ties;
+    localStorage.setItem('.ties', ties);
 }
+
+
+// function points (playerPoints) {
+//     if (winner === 'Player'){
+//         console.log(pointAlloc);
+//         return (playerPoints + 1);
+//     }    
+// }
+
+// const pointReturn = points(0);
+
+// document.querySelector('.playerScore').innerHTML = (`player score: ${pointReturn}`);
+// function scoreBoard(score) {
+//     if checkWinner = 'player'{
+//         pScore.innerHTML
+//     }
+// }
 game();
